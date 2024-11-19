386adc7a8c8671e0ef6e2834ab618939b42a561a
Donte Handy <donte.handy@gmail.com>
2024-11-19 09:10:48 -0500

    print even/odd based on grade

---
+++ code_challenges/mild_code_challenge.rb
@@ -0,0 +1,14 @@
+# ## Mild
+
+# Write a Ruby program that defines a variable that stores an Integer. 
+# The program should print out the String "even" if the Integer is even, 
+# and the String "odd" if the Integer is odd.
+
+grade = 3
+
+if grade.even?
+    puts "even"
+else
+    grade.odd?
+    puts "odd"
+end
